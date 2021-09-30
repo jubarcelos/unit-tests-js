09bfcafe74a5e11358489445af46dc33063e897b
Julia Barcelos <julaai@gmail.com>
2021-09-29 23:40:52 -0300

    Requisito 5 finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const dates = { 
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return dates;
+};
 
 module.exports = createStudent;

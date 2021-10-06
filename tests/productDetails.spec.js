892f0461fe8ec6f6eb3599bd63543bbfdb9ac06c
Julia Barcelos <julaai@gmail.com>
2021-10-06 00:48:38 -0300

    ajustes finais

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,16 +31,16 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     expect(typeof productDetails).toBe('function');
   });
   it('Verifica se o retorno da função é um array.', () => {
-    expect(typeof productDetails('Faca', 'Garfo')).toBe('object');
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true);
   });
   it ('Verifica os dois itens dentro do array retornado pela função são objetos.', () => {
-    expect(productDetails('Panela', 'Copo').length).toEqual(2)
+    expect(productDetails('Alcool gel', 'Máscara').length).toEqual(2)
   });
   it('Verifica se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
-    expect(productDetails('carro','moto')).not.toBe(productDetails('agua','fruta'));
+    expect(productDetails('Alcool gel', 'Máscara')[0] === productDetails('Alcool gel', 'Máscara')[1]).toBe(false);
   });
   it('Verifica se os dois productIds terminam com 123.', () => {
-    expect((productDetails('Jaleco','Sapato')[0].details.productId).endsWith('123')).toEqual(true);
-    expect((productDetails('Jaleco','Sapato')[1].details.productId).endsWith('123')).toEqual(true);
+    expect((productDetails('Alcool gel', 'Máscara')[0].details.productId).endsWith('123')).toEqual(true);
+    expect((productDetails('Alcool gel', 'Máscara')[1].details.productId).endsWith('123')).toEqual(true);
   });
   });
